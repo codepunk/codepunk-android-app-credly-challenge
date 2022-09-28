f37c8ca311f08a3b3edf0bf1c9208b7652525b69
Scott Slater <slaterama@gmail.com>
2022-09-28 10:10:41 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# codepunk-android-app-credly-challenge
---
+++ app/src/main/java/com/codepunk/credlychallenge/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.codepunk.credlychallenge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.codepunk.credlychallenge.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
