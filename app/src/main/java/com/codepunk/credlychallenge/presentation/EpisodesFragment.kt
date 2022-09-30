e6ac3d84b65ee2210eda0863fb972ef1f69e407a
Scott Slater <slaterama@gmail.com>
2022-09-30 12:46:47 -0400

    Episodes showing in recycler (without local caching yet)

--- app/src/main/java/com/codepunk/credlychallenge/presentation/EpisodesFragment.kt
+++ app/src/main/java/com/codepunk/credlychallenge/presentation/EpisodesFragment.kt
@@ -1,19 +1,28 @@
 package com.codepunk.credlychallenge.presentation
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.codepunk.credlychallenge.BuildConfig
+import com.codepunk.credlychallenge.R
 import com.codepunk.credlychallenge.databinding.FragmentEpisodesBinding
+import com.codepunk.credlychallenge.databinding.ItemEpisodeBinding
+import com.codepunk.credlychallenge.databinding.ItemShowBinding
 import com.codepunk.credlychallenge.domain.model.Episode
+import com.codepunk.credlychallenge.domain.model.Show
 import com.codepunk.credlychallenge.util.consume
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -28,6 +37,8 @@ class EpisodesFragment @Inject constructor() : Fragment() {
 
     private lateinit var binding: FragmentEpisodesBinding
 
+    private val episodeAdapter = EpisodeAdapter()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -39,6 +50,16 @@ class EpisodesFragment @Inject constructor() : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        with(binding.episodesRecycler) {
+            layoutManager = LinearLayoutManager(
+                requireContext(),
+                RecyclerView.VERTICAL,
+                false
+            )
+            addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+            adapter = episodeAdapter
+        }
 
         setUpCollection()
 
@@ -74,7 +95,7 @@ class EpisodesFragment @Inject constructor() : Fragment() {
 
     private fun onResult(result: Result<List<Episode>>) {
         result.onSuccess {
-            // TODO
+            episodeAdapter.episodes = it
         }
     }
 
@@ -86,4 +107,39 @@ class EpisodesFragment @Inject constructor() : Fragment() {
         }
     }
 
+    private class EpisodeViewHolder(val binding: ItemEpisodeBinding) : ViewHolder(binding.root) {
+        fun bind(episode: Episode) {
+            binding.episode = episode
+        }
+    }
+
+    private class EpisodeAdapter() : RecyclerView.Adapter<EpisodeViewHolder>() {
+
+        var episodes: List<Episode> = emptyList()
+            @SuppressLint("NotifyDataSetChanged")
+            set(value) {
+                field = value
+                notifyDataSetChanged()
+            }
+
+        override fun getItemCount(): Int = episodes.size
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EpisodeViewHolder {
+            val binding = DataBindingUtil.inflate<ItemEpisodeBinding>(
+                LayoutInflater.from(parent.context),
+                R.layout.item_episode,
+                parent,
+                false
+            )
+
+            return EpisodeViewHolder(binding)
+        }
+
+        override fun onBindViewHolder(holder: EpisodeViewHolder, position: Int) {
+            episodes.getOrNull(position)?.also { episode ->
+                holder.bind(episode)
+            }
+        }
+
+    }
 }
